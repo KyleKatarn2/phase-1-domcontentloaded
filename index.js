5442b8eab64914aa698bfd67d615f7320e3f3d01
Jonathan Campos <Malvyx5@gmail.com>
2023-05-31 18:31:21 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+    let text = document.querySelector('p').textContent = "This is really cool!";
+    return text;
+});
